f681545fb7dc763b71202f563f0db73b22a169bb
TanninOne <seppleviathan@gmx.de>
2018-12-03 17:48:25 +0100

    automate detection of sdk version

--- build_usvfs.js
+++ build_usvfs.js
@@ -4,8 +4,25 @@ if (process.env.BOOST_PATH === undefined) {
     return;
 }
 
+const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
+
+if (process.env.SDKVersion === undefined) {
+    try {
+        const sdkManifestPath = path.join(process.env['ProgramFiles(x86)'], 'Windows Kits', '10', 'SDKManifest.xml');
+	console.log('sdk manifest file at', sdkManifestPath);
+        const sdkManifest = fs.readFileSync(sdkManifestPath, { encoding: 'utf-8' });
+        const sdkver = sdkManifest
+            .split('\r\n')
+            .map(line => line.trim())
+            .find(line => line.startsWith('PlatformIdentity'))
+            .replace(/.*Version=([0-9.]*).*/, '$1');
+        process.env.SDKVersion = sdkver;
+    } catch (err) {
+        console.error('Failed to determine SDK version', err.message);
+    }
+}
 
 // find vs2017 installation
 const vsWhere = path.join(process.env['ProgramFiles(x86)'], 'Microsoft Visual Studio', 'Installer', 'vswhere.exe');
@@ -26,5 +43,13 @@ if (process.env.SDKVersion !== undefined) {
 }
 msbuild.overrideParams.push(`/P:BOOST_PATH=${process.env.BOOST_PATH}`);
 msbuild.overrideParams.push('/P:STAGING_BASE=..\\..\\usvfs_build');
+
+let first = true;
+msbuild.on('error', () => {
+    if (first) {
+        first = false;
+        msbuild.build();
+    }
+});
 
 msbuild.build();
